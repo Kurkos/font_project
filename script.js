102235424b5e09a03ebb17690069616a67ef874e
Kurkos <41972994+Kurkos@users.noreply.github.com>
2018-10-10 19:25:08 +0300

    update on margin
    update on margin

--- script.js
+++ script.js
@@ -1,44 +1,52 @@
 
 var paragrapgh = document.getElementById('paragrapgh');
-var letters = paragrapgh.innerText.split(''); // Spliting text
+var letters = paragrapgh.innerText.split(''); 
 var colors = [/*'red','green','blue','pink', 'gold', 'yellow', 'blueviolet'*/];
 var spans = [];
  
 for(var i = 0; i < letters.length; i++){
-  var color = colors[Math.floor(Math.random()*colors.length)] // geting random color from array;
+  var color = colors[Math.floor(Math.random()*colors.length)] // getting random color from array;
   var span = "<span class='spanTag' style='color: " + color + ";'>" + letters[i] + "</span>"
  
   spans.push(span);
 };
+
+paragrapgh.innerHTML = spans.join('');// setting colored spans as paragrapgh HTML
+
+
+ var spanTag = document.getElementsByClassName('spanTag');
  
-// setting colored spans as paragrapgh HTML
-paragrapgh.innerHTML = spans.join('');
- 
+ for(var i = 0; i < spanTag.length; i++) {
+  spanTag[i].addEventListener('mouseover', function(event) {
+		var randomNumber= Math.round(Math.random()*2);
+		
+			if (randomNumber==1){
+				event.target.style.marginLeft= '100px';
+			}else{
+				event.target.style.marginRight= '100px';
+			};
+	  setTimeout(function(){event.target.style.margin='0px'},7000);
+	  });
+}	
+
+ /* !Margin Left!
  var spanTag = document.getElementsByClassName('spanTag');
  for(var i = 0; i < spanTag.length; i++) {
   spanTag[i].addEventListener('mouseover', function(event) {
-  console.log(event.target);
-  event.target.style.marginLeft= '100px';
-  setTimeout(function(){event.target.style.marginLeft='0px'},7000)
-  });
+	 // console.log(event.target);
+	  event.target.style.marginLeft= '100px';
+	  setTimeout(function(){event.target.style.marginLeft='0px'},7000)
+	  });
  
 }	
+*/
  
- 
- 
- 
- /*
-var spans = document.getElementsByTagName('span');
-for(var i = 0; i < spans.length; i++) {
-  spans[i].addEventListener('mouseenter', function(e) {
-    e.target.className = 'hovered';
+ /* !CSS!
+  spans[i].addEventListener('mouseenter', function(event) {
+    event.target.className = 'hovered';
   });
- 
-  spans[i].addEventListener('mouseleave', function(e) {
-    e.target.className = '';
-  });
-}	
-		
+
+}		
 	*/	
 		
 		
