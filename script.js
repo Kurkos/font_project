349dfd982e82b51159428091407c6e6d9dfd5441
Kurkos <41972994+Kurkos@users.noreply.github.com>
2018-10-16 01:02:06 +0300

    mouse-out (add)

--- script.js
+++ script.js
@@ -17,16 +17,20 @@ paragrapgh.innerHTML = spans.join('');// setting colored spans as paragrapgh HTM
  var spanTag = document.getElementsByClassName('spanTag');
  
  for(var i = 0; i < spanTag.length; i++) {
-  spanTag[i].addEventListener('mouseover', function(event) {
-		var randomNumber= Math.round(Math.random()*2);
+	spanTag[i].addEventListener('mouseover', function(event) {
+		var randomNumber= Math.floor(Math.random()*2);
 		
-			if (randomNumber==1){
+			if (randomNumber==0){
 				event.target.style.marginLeft= '100px';
+				console.log(randomNumber);
 			}else{
 				event.target.style.marginRight= '100px';
+				console.log(randomNumber);
 			};
-	  setTimeout(function(){event.target.style.margin='0px'},7000);
 	  });
+	spanTag[i].addEventListener('mouseout', function(event) { 
+		setTimeout(function(){event.target.style.margin='0px'},7000);
+	});
 }	
 
  /* !Margin Left!
