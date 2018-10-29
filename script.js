a5c3c63979ee6671c2987810c10dfa1c27c033cd
Kurkos <41972994+Kurkos@users.noreply.github.com>
2018-10-29 18:46:12 +0200

    Update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var paragrapgh = document.getElementById('paragrapgh');
+var paragrapgh = document.querySelector('p');
 var letters = paragrapgh.innerText.split(''); 
 var colors = [/*'red','green','blue','pink', 'gold', 'yellow', 'blueviolet'*/];
 var spans = [];
@@ -10,14 +10,13 @@ for(var i = 0; i < letters.length; i++){
  
   spans.push(span);
 };
-
 paragrapgh.innerHTML = spans.join('');// setting colored spans as paragrapgh HTML
 
 
  var spanTag = document.getElementsByClassName('spanTag');
  
  for(var i = 0; i < spanTag.length; i++) {
-	spanTag[i].addEventListener('mouseover', function(event) {
+spanTag[i].addEventListener('mouseover', function(event) {
 		var randomNumber= Math.floor(Math.random()*2);
 		
 			if (randomNumber==0){
@@ -33,26 +32,4 @@ paragrapgh.innerHTML = spans.join('');// setting colored spans as paragrapgh HTM
 	});
 }	
 
- /* !Margin Left!
- var spanTag = document.getElementsByClassName('spanTag');
- for(var i = 0; i < spanTag.length; i++) {
-  spanTag[i].addEventListener('mouseover', function(event) {
-	 // console.log(event.target);
-	  event.target.style.marginLeft= '100px';
-	  setTimeout(function(){event.target.style.marginLeft='0px'},7000)
-	  });
- 
-}	
-*/
- 
- /* !CSS!
-  spans[i].addEventListener('mouseenter', function(event) {
-    event.target.className = 'hovered';
-  });
-
-}		
-	*/	
-		
-		
-		
-		
+	
